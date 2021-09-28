01fa51d4ec3482b8edb4c43101c845ad76ee79ed
Naveen Balachandar <naveenbalachandar@Naveens-MacBook-Pro.local>
2021-09-28 13:29:31 +0530

    fix all code-checker error and warnings from local/credentials.php

--- classes/local/credentials.php
+++ classes/local/credentials.php
@@ -20,7 +20,7 @@ defined('MOODLE_INTERNAL') || die();
 use mod_accredible\apiRest\apiRest;
 
 class credentials {
-	/**
+    /**
      * HTTP request apirest.
      * @var apirest
      */
@@ -28,7 +28,7 @@ class credentials {
 
     public function __construct($apirest = null) {
         // A mock apirest is passed when unit testing.
-        if($apirest) {
+        if ($apirest) {
             $this->apirest = $apirest;
         } else {
             $this->apirest = new apiRest();
@@ -36,165 +36,160 @@ class credentials {
     }
 
     /**
-	 * Create a credential given a user and an existing group
-	 * @param stdObject $user
-	 * @param int $group_id
-	 * @return stdObject
-	 */
-	function create_credential($user, $group_id, $event = null, $issued_on = null) {
-	    global $CFG;
+     * Create a credential given a user and an existing group
+     * @param stdObject $user
+     * @param int $groupid
+     * @return stdObject
+     */
+    public function create_credential($user, $groupid, $event = null, $issuedon = null) {
+        global $CFG;
 
-	    try {
-	        $credential = $this->apirest->create_credential(fullname($user), $user->email, $group_id, $issued_on);
+        try {
+            $credential = $this->apirest->create_credential(fullname($user), $user->email, $groupid, $issuedon);
 
-	        // Log an event now we've created the credential if possible.
-	        if ($event != null) {
-	            $certificate_event = \mod_accredible\event\certificate_created::create(array(
-	                                  'objectid' => $credential->credential->id,
-	                                  'context' => context_module::instance($event->contextinstanceid),
-	                                  'relateduserid' => $event->relateduserid,
-	                                  'issued_on' => $issued_on
-	                                ));
-	            $certificate_event->trigger();
-	        }
+            // Log an event now we've created the credential if possible.
+            if ($event != null) {
+                $certificateevent = \mod_accredible\event\certificate_created::create(array(
+                    'objectid' => $credential->credential->id,
+                    'context' => context_module::instance($event->contextinstanceid),
+                    'relateduserid' => $event->relateduserid,
+                    'issued_on' => $issuedon
+                ));
+                $certificateevent->trigger();
+            }
+            return $credential->credential;
+        } catch (\Exception $e) {
+            // Throw API exception.
+            // Include the achievement id that triggered the error.
+            // Direct the user to accredible's support.
+            // Dump the achievement id to debug_info.
+            throw new \moodle_exception('credentialcreateerror', 'accredible',
+                'https://help.accredible.com/hc/en-us', $user->email, $groupid);
+        }
+    }
 
-	        return $credential->credential;
+    /**
+     * Create a credential given a user and an existing group
+     * @param stdObject $user
+     * @param int $groupid
+     * @return stdObject
+     */
+    public function create_credential_legacy($user, $achievementname, $coursename,
+        $coursedescription, $courselink, $issuedon, $event = null) {
+        global $CFG;
+        try {
+            $credential = $this->apirest->create_credential_legacy(fullname($user),
+                $user->email, $achievementname, $issuedon, null, $coursename, $coursedescription, $courselink);
+            // Log an event now we've created the credential if possible.
+            if ($event != null) {
+                $certificateevent = \mod_accredible\event\certificate_created::create(array(
+                    'objectid' => $credential->credential->id,
+                    'context' => context_module::instance($event->contextinstanceid),
+                    'relateduserid' => $event->relateduserid
+                ));
+                $certificateevent->trigger();
+            }
+            return $credential->credential;
+        } catch (\Exception $e) {
+            // Throw API exception.
+            // Include the achievement id that triggered the error.
+            // Direct the user to accredible's support.
+            // Dump the achievement id to debug_info.
+            throw new \moodle_exception('credentialcreateerror', 'accredible',
+                'https://help.accredible.com/hc/en-us', $user->email, $achievementname);
+        }
+    }
 
-	    } catch (\Exception $e) {
-	        // Throw API exception.
-	        // Include the achievement id that triggered the error.
-	        // Direct the user to accredible's support.
-	        // Dump the achievement id to debug_info.
-	        throw new \moodle_exception('credentialcreateerror', 'accredible', 'https://help.accredible.com/hc/en-us', $user->email, $group_id);
-	    }
-	}
+    /**
+     * List all of the certificates with a specific achievement id
+     * @param string $groupid Limit the returned Credentials to a specific group ID.
+     * @param string|null $email Limit the returned Credentials to a specific recipient's email address.
+     * @return array[stdClass] $credentials
+     */
+    public function get_credentials($groupid, $email= null) {
+        global $CFG;
+        $pagesize = 50;
+        $page = 1;
 
-	/**
-	 * Create a credential given a user and an existing group
-	 * @param stdObject $user
-	 * @param int $group_id
-	 * @return stdObject
-	 */
-	function create_credential_legacy($user, $achievement_name, $course_name, $course_description, $course_link, $issued_on, $event = null){
-	    global $CFG;
+        // Maximum number of pages to request to avoid possible infinite loop.
+        $looplimit = 100;
+        try {
+            $loop = true;
+            $count = 0;
+            $credentials = array();
+            // Query the Accredible API and loop until it returns that there is no next page.
+            while ($loop === true) {
+                $credentialspage = $this->apirest->get_credentials($groupid, $email, $pagesize, $page);
+                foreach ($credentialspage->credentials as $credential) {
+                    $credentials[] = $credential;
+                }
 
-	    try {
-	        $credential = $this->apirest->create_credential_legacy(fullname($user), $user->email, $achievement_name, $issued_on, null, $course_name, $course_description, $course_link);
-	        // log an event now we've created the credential if possible
-	        if ($event != null) {
-	            $certificate_event = \mod_accredible\event\certificate_created::create(array(
-	                                  'objectid' => $credential->credential->id,
-	                                  'context' => context_module::instance($event->contextinstanceid),
-	                                  'relateduserid' => $event->relateduserid
-	                                ));
-	            $certificate_event->trigger();
-	        }
+                $page++;
+                $count++;
+                if ($credentialspage->meta->next_page === null || $count >= $looplimit) {
+                    // If the Accredible API returns that there
+                    // is no next page, end the loop.
+                    $loop = false;
+                }
+            }
+            return $credentials;
+        } catch (\Exception $e) {
+            // Throw API exception.
+            // Include the achievement id that triggered the error.
+            // Direct the user to accredible's support.
+            // Dump the achievement id to debug_info.
+            $exceptionparam = new \stdClass();
+            $exceptionparam->groupid = $groupid;
+            $exceptionparam->email = $email;
+            if (isset($credentialspage)) {
+                $exceptionparam->last_response = $credentialspage;
+            }
+            throw new \moodle_exception('getcredentialserror', 'accredible',
+                'https://help.accredible.com/hc/en-us', $exceptionparam);
+        }
+    }
 
-	        return $credential->credential;
+    /**
+     * Check's if a credential exists for an email in a particular group
+     * @param int $groupid
+     * @param String $email
+     * @return array[stdClass] || false
+     */
+    public function check_for_existing_credential($groupid, $email) {
+        global $CFG;
+        try {
+            $credentials = $this->apirest->get_credentials($groupid, $email);
 
-	    } catch (\Exception $e) {
-	        // Throw API exception.
-	        // Include the achievement id that triggered the error.
-	        // Direct the user to accredible's support.
-	        // Dump the achievement id to debug_info.
-	        throw new \moodle_exception('credentialcreateerror', 'accredible', 'https://help.accredible.com/hc/en-us', $user->email, $achievement_name);
-	    }
-	}
+            if ($credentials->credentials and $credentials->credentials[0]) {
+                return $credentials->credentials[0];
+            } else {
+                return false;
+            }
+        } catch (\Exception $e) {
+            // Throw API exception
+            // include the achievement id that triggered the error
+            // direct the user to accredible's support
+            // dump the achievement id to debug_info.
+            throw new \moodle_exception('groupsyncerror', 'accredible', 'https://help.accredible.com/hc/en-us', $groupid, $groupid);
+        }
+    }
 
-	/**
-	 * List all of the certificates with a specific achievement id
-	 *
-	 * @param string $group_id Limit the returned Credentials to a specific group ID.
-	 * @param string|null $email Limit the returned Credentials to a specific recipient's email address.
-	 * @return array[stdClass] $credentials
-	 */
-	function get_credentials($group_id, $email= null) {
-	    global $CFG;
+    /**
+     * Check's if a credential exists for an user in a particular group
+     * @param int $achievementid
+     * @param stdObject $user
+     * @return array[stdClass] || false
+     */
+    public function check_for_existing_certificate($achievementid, $user) {
+        global $DB;
+        $existingcertificate = false;
+        $certificates = $this->get_credentials($achievementid, $user->email);
 
-	    $page_size = 50;
-	    $page = 1;
-	    // Maximum number of pages to request to avoid possible infinite loop.
-	    $loop_limit = 100;
-
-	    try {
-	        $loop = true;
-	        $count = 0;
-	        $credentials = array();
-	        // Query the Accredible API and loop until it returns that there is no next page.
-	        while ($loop === true) {
-	            $credentials_page = $this->apirest->get_credentials($group_id, $email, $page_size, $page);
-
-	            foreach ($credentials_page->credentials as $credential) {
-	                $credentials[] = $credential;
-	            }
-
-	            $page++;
-	            $count++;
-
-	            if ($credentials_page->meta->next_page === null || $count >= $loop_limit) {
-	                // If the Accredible API returns that there
-	                // is no next page, end the loop.
-	                $loop = false;
-	            }
-	        }
-	        return $credentials;
-	    } catch (\Exception $e) {
-	        // Throw API exception.
-	        // Include the achievement id that triggered the error.
-	        // Direct the user to accredible's support.
-	        // Dump the achievement id to debug_info.
-	        $exceptionparam = new \stdClass();
-	        $exceptionparam->group_id = $group_id;
-	        $exceptionparam->email = $email;
-	        if(isset($credentials_page)){
-	        	$exceptionparam->last_response = $credentials_page;
-	        }
-	        throw new \moodle_exception('getcredentialserror', 'accredible', 'https://help.accredible.com/hc/en-us', $exceptionparam);
-	    }
-	}
-
-	/**
-	 * Check's if a credential exists for an email in a particular group
-	 * @param int $group_id
-	 * @param String $email
-	 * @return array[stdClass] || false
-	 */
-
-	function check_for_existing_credential($group_id, $email) {
-	    global $CFG;
-	    try {
-	        $credentials = $this->apirest->get_credentials($group_id, $email);
-
-	        if ($credentials->credentials and $credentials->credentials[0]) {
-	            return $credentials->credentials[0];
-	        } else {
-	            return false;
-	        }
-	    } catch (\Exception $e) {
-	        // throw API exception
-	          // include the achievement id that triggered the error
-	          // direct the user to accredible's support
-	          // dump the achievement id to debug_info
-	          throw new \moodle_exception('groupsyncerror', 'accredible', 'https://help.accredible.com/hc/en-us', $group_id, $group_id);
-	    }
-	}
-
-	/**
-	 * Check's if a credential exists for an user in a particular group
-	 * @param int $achievement_id
-	 * @param stdObject $user
-	 * @return array[stdClass] || false
-	 */
-	function check_for_existing_certificate($achievement_id, $user) {
-	    global $DB;
-	    $existing_certificate = false;
-	    $certificates = $this->get_credentials($achievement_id, $user->email);
-
-	    foreach ($certificates as $certificate) {
-	        if ($certificate->recipient->email == $user->email) {
-	            $existing_certificate = $certificate;
-	        }
-	    }
-	    return $existing_certificate;
-	}
+        foreach ($certificates as $certificate) {
+            if ($certificate->recipient->email == $user->email) {
+                $existingcertificate = $certificate;
+            }
+        }
+        return $existingcertificate;
+    }
 }

60d4b08822f94328ebd9e8136cd0f6e7f57215aa
kei178 <keisuke.inaba@hey.com>
2021-12-14 18:37:09 +0900

    Update version for QA

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021120900; // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2021121400; // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051200; // Requires this Moodle version.
 $plugin->cron      = 0; // Period for cron to check this module (secs).
 $plugin->component = 'mod_accredible';

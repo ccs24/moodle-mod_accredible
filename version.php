d8cbabdebe2eaa8945492298834ae9df32863347
octaviorequena <octavio@accredible.com>
2022-07-28 10:44:51 +0200

    bump version to 2.2

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022072700; // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2022072800; // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051200; // Requires this Moodle version.
 $plugin->cron      = 0; // Period for cron to check this module (secs).
 $plugin->component = 'mod_accredible';
